fd7d82b122f9e5c74fec2e1863a8a67aa97b8d1f
AtharvaJoshi21 <joshi.atharva92@hotmail.com>
2019-06-05 22:11:18 +0530

    Secondary Window Creation Display Chrome DevTools

--- main.js
+++ main.js
@@ -2,12 +2,13 @@ const electron = require('electron');
 const url = require('url');
 const path = require('path');
 
-const {app, BrowserWindow, Menu} = electron;
+const { app, BrowserWindow, Menu } = electron;
 
 let mainWindow;
+let addWindow;
 
 // Listen for app to be ready
-app.on('ready', function(){
+app.on('ready', function () {
     // Create new window
     mainWindow = new BrowserWindow({});
 
@@ -18,6 +19,11 @@ app.on('ready', function(){
         slashes: true
     }));
 
+    // To close the entire application when main window is closed
+    mainWindow.on('closed', function () {
+        app.quit();
+    });
+
     // Build menu from template
     const mainMenu = Menu.buildFromTemplate(mainMenuTemplate);
 
@@ -25,13 +31,38 @@ app.on('ready', function(){
     Menu.setApplicationMenu(mainMenu);
 });
 
+// Handle create add window
+function createAddWindow() {
+    // Create new add window
+    addWindow = new BrowserWindow({
+        width: 300,
+        height: 200,
+        title: 'Add Shopping List Item'
+    });
+
+    // Load the html into window
+    addWindow.loadURL(url.format({
+        pathname: path.join(__dirname, 'addWindow.html'),
+        protocol: 'file:',
+        slashes: true
+    }));
+
+    // Handle garbage collection
+    addWindow.on('close', function () {
+        addWindow = null;
+    });
+}
+
 // Create menu template
 const mainMenuTemplate = [
     {
         label: 'File',
         submenu: [
             {
-                label: 'Add Item'
+                label: 'Add Item',
+                click() {
+                    createAddWindow();
+                }
             },
             {
                 label: 'Clear Items'
@@ -39,10 +70,34 @@ const mainMenuTemplate = [
             {
                 label: 'Quit',
                 accelerator: process.platform == 'darwin' ? 'Command+Q' : 'Ctrl+Q',
-                click(){
+                click() {
                     app.quit();
                 }
             }
         ]
     }
 ];
+
+// Handle the MacOS menu item issue
+if (process.platform == 'darwin') {
+    mainMenuTemplate.unshift({});
+}
+
+// Display developer tools if the environment is not production
+if (process.env.NODE_ENV != 'production') {
+    mainMenuTemplate.push({
+        label: 'Developer Tools',
+        submenu: [
+            {
+                label: 'Toggle DevTools',
+                accelerator: process.platform == 'darwin' ? 'Command+I' : 'Ctrl+I',
+                click(item, focussedWindow) {
+                    focussedWindow.toggleDevTools();
+                }
+            },
+            {
+                role: 'reload'
+            }
+        ]
+    });
+}
